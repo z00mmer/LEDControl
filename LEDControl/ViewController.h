00f6ff08863c705b39fbab19a8db2e088adaa5a9
Sean <z00mer@me.com>
2017-09-21 15:33:33 +0900

    Initial Commit

---
+++ LEDControl/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LEDControl
+//
+//  Created by Sean on 2017. 9. 21..
+//  Copyright © 2017년 Sean. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
